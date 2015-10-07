e2c7f9e127015310e2e3d5ea4eb9f0fe069d1cd0
Robert Simpson <robert_simpson@hms.harvard.edu>
2015-10-07 14:39:47 -0400

    Update readme file; add body tag to index file.

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <h1> Hello World 2!</h1>
 
-<body>Test of new local server on MacMini Desktop</body> 
+<body>Test of new local server on MacMini Desktop
+
+<h2> Sadly, my MacBookPro laptop is still not working after wiping the HD, reinstalling new OS X 
+and restoring from 9/15/15 backup. HMS Apple technician is now looking at hardware malfunction. It 
+could be related to the battery.  
+<br>
+<a href="https://discussions.apple.com/thread/7248051">Apple Support Community
+ - mouse moving on its own</a> 
+
+
+</body> 
+
--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 October 7, 2015
+
+Added body tag and explanation of MacBookPro malfunction. 
